67b8fc2de2618a485d08bb630d0686ad01168d5a
kkazm <s14005@pjwstk.edu.pl>
2021-10-18 16:47:34 +0200

    Initial commit.

---
+++ src/main/java/restaurant/com/deliverydriversqueuemanager/DeliveryDriversQueueManagerApplication.java
@@ -0,0 +1,13 @@
+package restaurant.com.deliverydriversqueuemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryDriversQueueManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryDriversQueueManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/restaurant/com/deliverydriversqueuemanager/DeliveryDriversQueueManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package restaurant.com.deliverydriversqueuemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryDriversQueueManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
