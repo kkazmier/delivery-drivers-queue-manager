09f3a1120b66c32529756bb720e59b2924b32d47
kkazm <s14005@pjwstk.edu.pl>
2021-10-23 12:43:15 +0200

    Add findAll().

--- src/main/java/restaurant/com/deliverydriversqueuemanager/repository/UserRepository.java
+++ src/main/java/restaurant/com/deliverydriversqueuemanager/repository/UserRepository.java
@@ -3,6 +3,9 @@ package restaurant.com.deliverydriversqueuemanager.repository;
 import org.springframework.data.repository.CrudRepository;
 import restaurant.com.deliverydriversqueuemanager.model.User;
 
+import java.util.List;
+
 public interface UserRepository extends CrudRepository<User, Long> {
     User findByUsername(String username);
+    List<User> findAll();
 }
