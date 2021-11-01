76c900743abd7203513c3e961cba9454d8424f62
kkazm <s14005@pjwstk.edu.pl>
2021-11-01 18:39:53 +0100

    Change letter size.

--- src/main/java/restaurant/com/deliverydriversqueuemanager/controller/DriverController.java
+++ src/main/java/restaurant/com/deliverydriversqueuemanager/controller/DriverController.java
@@ -1,4 +1,4 @@
-package restaurant.com.deliverydriversqueuemanager.Controller;
+package restaurant.com.deliverydriversqueuemanager.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
