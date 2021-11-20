842bad2ab568f7fbb182a9dfb60cea087bc60f9d
kkazm <s14005@pjwstk.edu.pl>
2021-11-20 16:34:43 +0100

    Add log.

--- src/main/java/restaurant/com/deliverydriversqueuemanager/handler/SimpleLogoutSuccessHandler.java
+++ src/main/java/restaurant/com/deliverydriversqueuemanager/handler/SimpleLogoutSuccessHandler.java
@@ -2,12 +2,9 @@ package restaurant.com.deliverydriversqueuemanager.handler;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.springframework.stereotype.Component;
-import restaurant.com.deliverydriversqueuemanager.model.User;
-import restaurant.com.deliverydriversqueuemanager.util.ActiveUserStore;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -18,13 +15,16 @@ import java.io.IOException;
 @Component("simpleLogoutSuccessHandler")
 public class SimpleLogoutSuccessHandler implements LogoutSuccessHandler {
     private static final Logger logger = LoggerFactory.getLogger(SimpleLogoutSuccessHandler.class);
+
     @Override
     public void onLogoutSuccess(HttpServletRequest request,
                                 HttpServletResponse response, Authentication authentication)
             throws IOException, ServletException {
+        logger.info("onLogoutSuccess()");
         HttpSession session = request.getSession();
-        if (session != null){
-            logger.info("Remove " + session.getAttribute("user") + " from session.");
+        if (session != null) {
+            String username = authentication.getName();
+            logger.info("Logout: " + username);
             session.removeAttribute("user");
         }
     }
