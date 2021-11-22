71eae3ded0d0368ee80b15337a7e14620e1617ca
kkazm <s14005@pjwstk.edu.pl>
2021-11-22 21:24:24 +0100

    Fix sec:authorize="isAuthenticated()" that don't work.

--- src/main/java/restaurant/com/deliverydriversqueuemanager/config/AppConfig.java
+++ src/main/java/restaurant/com/deliverydriversqueuemanager/config/AppConfig.java
@@ -3,6 +3,7 @@ package restaurant.com.deliverydriversqueuemanager.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.thymeleaf.extras.java8time.dialect.Java8TimeDialect;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 import org.thymeleaf.spring5.ISpringTemplateEngine;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.templateresolver.ITemplateResolver;
@@ -19,6 +20,7 @@ public class AppConfig {
         SpringTemplateEngine engine = new SpringTemplateEngine();
         engine.addDialect(new Java8TimeDialect());
         engine.setTemplateResolver(templateResolver);
+        engine.addDialect(new SpringSecurityDialect());
         return engine;
     }
 }
