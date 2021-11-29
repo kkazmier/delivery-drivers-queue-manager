394d2f6d57002e4998adff0a593019fd5ec6532c
kkazm <s14005@pjwstk.edu.pl>
2021-11-29 16:56:54 +0100

    Add updateWorkplace(...).

--- src/main/java/restaurant/com/deliverydriversqueuemanager/repository/UserRepository.java
+++ src/main/java/restaurant/com/deliverydriversqueuemanager/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package restaurant.com.deliverydriversqueuemanager.repository;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 import restaurant.com.deliverydriversqueuemanager.model.User;
 
 import java.util.List;
@@ -9,4 +12,9 @@ public interface UserRepository extends CrudRepository<User, Long> {
     User findByUsername(String username);
     List<User> findAll();
     void deleteAll();
+
+    @Modifying
+    @Transactional
+    @Query("update User u set u.workPlace = :workplace where u.username = :username")
+    void updateWorkplace(String username, String workplace);
 }
