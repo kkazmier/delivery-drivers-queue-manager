36d891753879e44b8fbf7123d0cdad1fb2fd0a09
kkazm <s14005@pjwstk.edu.pl>
2021-11-29 16:54:09 +0100

    Fix typo.

--- src/main/java/restaurant/com/deliverydriversqueuemanager/model/Workplace.java
+++ src/main/java/restaurant/com/deliverydriversqueuemanager/model/Workplace.java
@@ -1,6 +1,6 @@
 package restaurant.com.deliverydriversqueuemanager.model;
 
-public class WorkPlace {
+public class Workplace {
     public static final String PIZZA = "PIZZA";
     public static final String BURGER = "BURGER";
     public static final String NOWHERE = "NOWHERE";
