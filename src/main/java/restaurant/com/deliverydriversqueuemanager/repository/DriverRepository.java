5279c5ee02852d1a5b01c6e1f0b3cf22b0cb93e0
kkazm <s14005@pjwstk.edu.pl>
2021-11-30 14:07:11 +0100

    Swap lines.

--- src/main/java/restaurant/com/deliverydriversqueuemanager/repository/DriverRepository.java
+++ src/main/java/restaurant/com/deliverydriversqueuemanager/repository/DriverRepository.java
@@ -1,5 +1,6 @@
 package restaurant.com.deliverydriversqueuemanager.repository;
 
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -12,8 +13,8 @@ import java.util.List;
 public interface DriverRepository extends CrudRepository<Driver, Long> {
     Driver save(Driver driver);
     List<Driver> findAll();
-    @Query(value = "select * from drivers join users on drivers.id=users.driver_id where users.username = :username", nativeQuery = true)
-    //@Query(value = "select * from drivers where drivers.id=22", nativeQuery = true)
-    Driver findDriverByUsername(@Param("username") String username);
     void deleteAll();
+
+    @Query(value = "select * from drivers join users on drivers.id=users.driver_id where users.username = :username", nativeQuery = true)
+    Driver findDriverByUsername(@Param("username") String username);
 }
